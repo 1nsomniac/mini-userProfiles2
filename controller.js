28adadc5eee4ab6dd6e9a3b0b4630b7510d856a3
1nsomniac <samthakala@gmail.com>
2016-11-16 15:02:03 -0700

    I finished this addition to the mini-userProfiles. Now it grabs the data from a webpage, which is SWEEEET. It took a little to get the promises stuff down.

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  $scope.currentPage = 2;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	var promise = mainService.getUsers($scope.currentPage)
+
+    promise.then(function(response) {
+       $scope.users = response.data.data;
+       $scope.totalPages = response.data.total_pages
+    })
   }
-
-
   $scope.getUsers();
+
+  $scope.next= function () {
+    if($scope.totalPages > $scope.currentPage){
+      $scope.currentPage++;
+      $scope.getUsers();
+    }
+  }
+  $scope.back= function () {
+    if($scope.currentPage > 1){
+      $scope.currentPage--;
+      $scope.getUsers();
+    }
+  }
 
 });
